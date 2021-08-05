af5f9e2ee6a22baa53bcd2db25a81448974abd22
Benjamin <benjamin.pasic1@gmail.com>
2021-08-05 20:44:00 +0200

    Homepage added

--- src/home.js
+++ src/home.js
@@ -3,7 +3,38 @@ import {createElement, addClassToElement} from './webpage.js';
 let body = document.querySelector("body");
 
 function loadHome(){
-    
+
+    let container = createElement("div");
+    addClassToElement(container, "home-container");
+
+    let restaurantName = createElement("h1");
+    restaurantName.textContent = "Little Rome Italian Ristorante";
+    addClassToElement(restaurantName, "restaurant-name");
+
+    let restaurantSubtitle = createElement("h2");
+    restaurantSubtitle.textContent = "A rare experience in taste";
+    addClassToElement(restaurantSubtitle, "restaurant-subtitle");
+
+    let buttonContainer = createElement("div");
+    addClassToElement(buttonContainer, "menu-button-container");
+
+    let menuButton = createElement("button");
+    menuButton.textContent = "Menu";
+    addClassToElement(menuButton, "menu-button");
+
+    let contactButton = createElement("button");
+    contactButton.textContent = "Contact";
+    addClassToElement(contactButton, "contact-button");
+
+    buttonContainer.appendChild(menuButton);
+    buttonContainer.appendChild(contactButton);
+
+    container.appendChild(restaurantName);
+    container.appendChild(restaurantSubtitle);
+    container.appendChild(buttonContainer);
+
+    body.appendChild(container);
+
 }
 
 export {loadHome};
